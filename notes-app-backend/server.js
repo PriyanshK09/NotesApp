190e9849320868963da5b477942e3ec54b402c85
PriyanshK09 <priyanshkhare4905@gmail.com>
2024-07-08 22:51:19 +0530

    chore: Redirect root URL to notesmee.netlify.app

--- notes-app-backend/server.js
+++ notes-app-backend/server.js
@@ -19,6 +19,10 @@ mongoose.connect(process.env.MONGODB_URI, {
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.error('MongoDB connection error:', err));
 
+app.get('/', (req, res) => {
+    res.redirect('https://notesmee.netlify.app/');
+});
+
 app.use('/api/auth', authRoutes);
 app.use('/api/notes', noteRoutes);
 
