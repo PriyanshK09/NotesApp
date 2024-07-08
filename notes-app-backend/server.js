5f8d161999245bbc06857bcc9351d90f4873755d
PriyanshK09 <priyanshkhare4905@gmail.com>
2024-07-08 23:17:56 +0530

    chore: Update npm dependencies and add path module to package.json

--- notes-app-backend/server.js
+++ notes-app-backend/server.js
@@ -1,39 +1,38 @@
 // server.js
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require('path');
+require("dotenv").config();
 
-const authRoutes = require('./routes/auth');
-const noteRoutes = require('./routes/notes');
+const authRoutes = require("./routes/auth");
+const noteRoutes = require("./routes/notes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.error('MongoDB connection error:', err));
-
-app.get('/health', (req, res) => {
-    res.status(200).send('OK');
-  });
-
-app.get('/', (req, res) => {
-    res.send(`
-      <script>
-        alert('Backend server is running');
-        window.location.href = 'https://notesmee.netlify.app/';
-      </script>
-    `);
+app.get("/", (req, res) => {
+    // user with ip address prited to console for debugging
+    console.log(req.ip);
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.use('/api/auth', authRoutes);
-app.use('/api/notes', noteRoutes);
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/notes", noteRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
