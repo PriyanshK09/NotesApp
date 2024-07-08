b5821b74ec649e3cb462e5bc4acd9e3ec85c63ef
PriyanshK09 <priyanshkhare4905@gmail.com>
2024-07-09 00:20:32 +0530

    chore: Update server.js to remove unnecessary code and improve code organization

--- notes-app-backend/server.js
+++ notes-app-backend/server.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const path = require('path');
 require("dotenv").config();
 
 const authRoutes = require("./routes/auth");
@@ -13,26 +12,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    // user with ip address prited to console for debugging
-    console.log(req.ip);
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+app.use("/api/auth", authRoutes);
+app.use("/api/notes", noteRoutes);
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
 app.get("/health", (req, res) => {
   res.status(200).send("OK");
 });
-
-app.use("/api/auth", authRoutes);
-app.use("/api/notes", noteRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
