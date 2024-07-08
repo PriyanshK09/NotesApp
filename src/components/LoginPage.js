b9b60e7f1512dac4d0f1aa91f561941aaf2f8af6
PriyanshK09 <priyanshkhare4905@gmail.com>
2024-07-09 00:07:54 +0530

    chore: Update API endpoint in LoginPage.js

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -16,7 +16,7 @@ function LoginPage({ setLoading }) {
     setLoading(true);
     try {
       const endpoint = isLogin ? '/api/auth/login' : '/api/auth/register';
-      const response = await axios.post(`http://localhost:5000${endpoint}`, { username, password });
+      const response = await axios.post(`https://notesappbackend-ah4g.onrender.com${endpoint}`, { username, password });
       if (response.data.token) {
         localStorage.setItem('token', response.data.token);
         localStorage.setItem('username', response.data.username);
